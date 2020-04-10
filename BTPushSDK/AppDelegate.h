2915f831e8ec689fa7c4e0b3b321cd2b2e2a083d
luxi.zhu@alphayee.com <zhuluxigitlab>
2020-04-10 15:33:42 +0800

    first commit

---
+++ BTPushSDK/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BTPushSDK
+//
+//  Created by NewBoy on 2020/4/10.
+//  Copyright © 2020 newboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Pods/Target Support Files/Pods-BTPushSDK/Pods-BTPushSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BTPushSDK : NSObject
+@end
+@implementation PodsDummy_Pods_BTPushSDK
+@end
